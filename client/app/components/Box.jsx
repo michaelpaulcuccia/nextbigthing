db893ce52b9eabf455f12b08030058904e641d5d
Michael Cuccia <michael.cuccia@egplusww.com>
2023-11-14 11:23:37 -0500

    progress capture

--- client/app/components/Box.jsx
+++ client/app/components/Box.jsx
@@ -1,34 +1,67 @@
-"use strict";
-import styled, { keyframes } from "styled-components";
-
-const myAnim = keyframes`
-  0% {
-		  opacity: 0;
-		  transform: translateX(250px);
-	  }
-
-	  100% {
-		  opacity: 1;
-		  transform: translateX(0);
-	}
-`;
+"use client";
+import styled from "styled-components";
+import Image from "next/image";
+import * as breakpoints from '../constants/breakpoints';
 
 const Root = styled.div`
-  margin: 8px 6px;
-  padding: 8px;
-  border-radius: 8px;
+  width: 30rem;
+  height: 15rem;
+  border-radius: 15rem 15rem 0 0;
   color: ${(props) => props.color || "black"};
   background: ${(props) => props.background || "black"};
   border: ${(props) => props.border || "black"};
-  animation: ${myAnim} 1s ease-in-out 0s 1 normal forwards;
+  position: relative;
 `;
 
+const Overlay = styled.div`
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  * {
+    margin: 0 auto;
+  }
+
+  h2, p {
+    letter-spacing: 1.5px;
+
+    @media (min-width: ${breakpoints.MOBILE}) {
+      letter-spacing: 0;
+    }
+  }
+
+  h2 {
+    padding-top: 3px;
+  }
+
+  table {
+    padding-top: 28px;
+    width: 24rem;
+    text-align: center;
+  }
+
+`;
+
+
 export default function Box({
-  children,
   className,
   border,
   background,
   color,
+  activityType,
+  image,
+  date,
+  time,
+  stat1,
+  stat2,
+  stat3,
+  statData1,
+  statData2,
+  statData3
 }) {
   return (
     <Root
@@ -37,7 +70,23 @@ export default function Box({
       background={background}
       color={color}
     >
-      {children}
+      <Overlay>
+          <Image src={image} width={25} height={25} alt=""/>
+          <h2>{activityType}</h2>
+          <p>{date}, {time}</p>
+          <table>
+            <tr>
+              <th>{statData1}</th>
+              <th>{statData2}</th>
+              <th>{statData3}</th>
+            </tr>
+            <tbody>
+              <td>{stat1}</td>
+              <td>{stat2}</td>
+              <td>{stat3}</td>
+            </tbody>
+          </table>
+      </Overlay>
     </Root>
   );
 }
--- client/app/page.js
+++ client/app/page.js
@@ -2,19 +2,26 @@
 
 import Box from "./components/Box";
 import * as Colors from "./constants/colors";
+import runner from './assets/icons/runner.svg';
 
 export default function Home() {
   return (
     <>
-      <Box border="white" color="white" background={Colors.BLEND_ONE}>
-        hello world 1
-      </Box>
-      <Box border="white" color="white" background={Colors.BLEND_TWO}>
-        hello world 2
-      </Box>
-      <Box border="white" color="white" background={Colors.BLEND_THREE}>
-        hello world 3
-      </Box>
+      <Box 
+        border="white" 
+        color="white" 
+        background={Colors.BLEND_ONE} 
+        activityType="Walking"
+        image={runner}
+        date='11/14/23'
+        time='0600'
+        stat1="50.5"
+        stat2="100"
+        stat3='0.03'
+        statData1="distance"
+        statData2="avg. heart rate"
+        statData3="elevation"
+      />
     </>
   );
 }
